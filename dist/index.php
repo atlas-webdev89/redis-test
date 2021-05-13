c572b3c0d2c9bd4bf704fd280696ff1d70f4c660
Atlas_WebDev <web.dev89@yandex.by>
2021-05-13 23:16:37 +0300

    redis start

--- dist/index.php
+++ dist/index.php
@@ -1,3 +1,34 @@
 <?php
+define('REDIS', getenv('REDIS'));
+require_once 'vendor/autoload.php';
+try {
 
-phpinfo();
+    $redis = new Predis\Client(
+                    [
+                        "scheme" => "tcp",
+                        "host" => REDIS,
+                        "port" => 6379
+                    ]
+            );
+$redis->set("key", "data");
+//$redis->expire("key", 60);
+
+
+
+$stdout = fopen('php://stdout', 'w');
+while($redis->exists("key")) {
+    
+            
+                    fwrite($stdout, sprintf("Hello wordls\n"));
+               
+        sleep(1);
+    }
+     fclose($stdout);
+}
+
+catch (Exception $e) {
+
+    echo ($e->getMessage());
+
+}
+
---
+++ dist/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fbec056050bb2fa16caf40c613bb775370792d8d',
+    'name' => 'atlas/redis',
+  ),
+  'versions' => 
+  array (
+    'atlas/redis' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fbec056050bb2fa16caf40c613bb775370792d8d',
+    ),
+    'pimple/pimple' => 
+    array (
+      'pretty_version' => 'v3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '86406047271859ffc13424a048541f4531f53601',
+    ),
+    'predis/predis' => 
+    array (
+      'pretty_version' => 'v1.1.7',
+      'version' => '1.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b240daa106d4e02f0c5b7079b41e31ddf66fddf8',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8622567409010282b7aeebe4bb841fe98b58dcaf',
+    ),
+  ),
+);
